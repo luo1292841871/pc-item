7269ef10296ea95bd5894d32f80829b10e3cbfce
luoluo <1292841871@qq.com>
2019-07-09 19:02:26 +0800

    pc项目

---
+++ js/index.js
@@ -0,0 +1,12 @@
+window.onload = function () {
+
+
+    var headerArrow = document.querySelector('.header-main-arrow');
+    var headerNavBottom = document.querySelectorAll('.header-main-bottom');
+    var headerNavLi = document.querySelectorAll('.header-main-nav li');
+
+    var headerNavHome = headerNavLi[0];
+
+    var ARROW
+
+}
